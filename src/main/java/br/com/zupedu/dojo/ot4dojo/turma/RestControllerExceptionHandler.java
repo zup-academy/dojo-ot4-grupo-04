32d4386847650fcb77872c79a465e4de00f6fbfc
Lais Mukai <lais.mukai@zup.com.br>
2021-05-27 15:49:33 -0300

    implementado mensagem de validação customizada

--- src/main/java/br/com/zupedu/dojo/ot4dojo/turma/RestControllerExceptionHandler.java
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/turma/RestControllerExceptionHandler.java
@@ -2,9 +2,11 @@ package br.com.zupedu.dojo.ot4dojo.turma;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,10 +20,19 @@ public class RestControllerExceptionHandler {
     private MessageSource messageSource;
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public List<ValidateErroroOutPut> listaErros (MethodArgumentNotValidException exception){
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public List<ValidateErroroOutPut> listaErros(MethodArgumentNotValidException exception) {
 
         List<ValidateErroroOutPut> list = new ArrayList<>();
-        List<FieldError> nomeList = exception.getBindingResult().getFieldError();
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(erro -> {
+            String mensagem = messageSource.getMessage(erro, LocaleContextHolder.getLocale());
+            ValidateErroroOutPut validacaoErro = new ValidateErroroOutPut(mensagem,erro.getField());
+
+            list.add(validacaoErro);
+
+        });
+        return list;
 
     }
 
